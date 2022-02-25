6216f61139e823a2791e316c7f887fd00c267e33
James Denning <j_v_denning@hotmail.com>
2022-02-25 11:27:03 +0000

    Tweak Get-Projects function

--- src/Public/Get-Projects.ps1
+++ src/Public/Get-Projects.ps1
@@ -6,5 +6,5 @@ function Get-Projects {
         $RefreshCache = $false
     )
 
-    Get-CachedScriptBlockResult -Key "allprojects" -RefreshCache:$RefreshCache -ScriptBlock { (Invoke-AzCliCommand "devops project list --detect false --get-default-team-image-url false" -OutputType PSObject).value }
+    Get-CachedScriptBlockResult -Key "allprojects" -RefreshCache:$RefreshCache -ScriptBlock { (Invoke-AzCliCommand "devops project list --detect false --get-default-team-image-url false" -OutputType PSObject).value   }
 }
